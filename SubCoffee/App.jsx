8d9824b6cfebcdb141147b3e9070831d6fb18ed9
katiuskizzz <munozkatalinavalderrama@gmail.com>
2024-04-26 22:34:23 -0500

    para emepezar a trabajar

---
+++ SubCoffee/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TabNavigation from "./Screen/TabsNavigation";
+
+const Stack = createNativeStackNavigator()
+
+const App = () => {
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name= 'SubCoffee' component={TabNavigation}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+export default App
